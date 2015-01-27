6ae5a7f3a056184b0a917171bb9eb506b24c0d9b
Noha Alon <nohalon@gmail.com>
2015-01-27 14:41:05 -0800

    First Commit

---
+++ StudyIn_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StudyIn_iOS
+//
+//  Created by Noha Alon on 1/27/15.
+//  Copyright (c) 2015 Noha Alon & Lucas David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
